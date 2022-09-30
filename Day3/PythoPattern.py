cada9f41b73142dd6f74155c321f75f71a39f062
Nayak0702 <vijethanayak07@gmail.com>
2022-10-01 00:11:44 +0530

    first commit

---
+++ Day3/PythoPattern.py
@@ -0,0 +1,19 @@
+# Design Patterns Learning
+#PROGRAM #1
+rows = 6
+for i in range(rows):
+    for j in range(i):
+        print(i, end = "")
+    print("")
+ 
+i=5
+for j in range(i):
+    print(j)
+
+rows = 7
+for i in range(rows):
+    for j in range(i):
+        print(i, end = '')
+    print("")
+
+    
